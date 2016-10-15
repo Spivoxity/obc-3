7dfb1aca4bd57f26f4da3757485dcc530c05a871
Mike Spivey <mike@cs.ox.ac.uk>
2016-10-15 23:14:30 +0100

    Compatibility code for ocaml < 4.02

---
+++ compat/bytes.ml
@@ -0,0 +1,17 @@
+let create n = String.create n
+
+let of_string s = s
+
+let get s i = String.get s i
+
+let set s i v = String.set s i v
+
+let unsafe_set s i v = String.unsafe_set s i v
+
+let to_string s = s
+
+let sub_string s i j = String.sub s i j
+
+let length s = String.length s
+
+let fill s i n c = String.fill s i n c
