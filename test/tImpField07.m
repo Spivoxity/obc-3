cee775b09a9ae07bd9b13b65523d77e684b65779
Mike Spivey <mike@cs.ox.ac.uk>
2016-11-15 21:00:42 +0000

    Bugfix: NEGD missing from vm386
    --HG--
    branch : rel-3.0

--- test/tImpField07.m
+++ test/tImpField07.m
@@ -3,10 +3,11 @@ IMPORT t := xTypes07, Out;
 VAR s: t.rec;
 BEGIN
   s.x := 3;
-  Out.Int(s.x, 0)
+  Out.Int(s.x, 0); Out.Ln
 END tImpField07.
 
 (*<<
+3
 >>*)
 
 (*[[
@@ -23,12 +24,14 @@ PROC tImpField07.%main 0 3 0
 CONST 3
 GLOBAL tImpField07.s
 STNW 4
-!   Out.Int(s.x, 0)
+!   Out.Int(s.x, 0); Out.Ln
 CONST 0
 GLOBAL tImpField07.s
 LDNW 4
 GLOBAL Out.Int
 CALL 2
+GLOBAL Out.Ln
+CALL 0
 RETURN
 END
 
